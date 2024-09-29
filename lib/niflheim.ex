39e729397074d9dca7c16aca7deec4969c8d9637
Artur Sulej <artur@fresha.com>
2024-09-29 10:38:58 +0200

    mix new niflheim

---
+++ lib/niflheim.ex
@@ -0,0 +1,18 @@
+defmodule Niflheim do
+  @moduledoc """
+  Documentation for `Niflheim`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Niflheim.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/niflheim_test.exs
@@ -0,0 +1,8 @@
+defmodule NiflheimTest do
+  use ExUnit.Case
+  doctest Niflheim
+
+  test "greets the world" do
+    assert Niflheim.hello() == :world
+  end
+end
