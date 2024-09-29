9feeec97394390515fb910d75b348c9ac27d978d
Artur Sulej <artur@fresha.com>
2024-09-29 11:03:46 +0200

    mix rustler.new

---
+++ lib/nif_demo.ex
@@ -0,0 +1,6 @@
+defmodule Niflheim.NifDemo do
+  use Rustler, otp_app: :niflheim, crate: "niflheim_nifdemo"
+
+  # When your NIF is loaded, it will override this function.
+  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+end
