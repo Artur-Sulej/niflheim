51b6f43a3d75dc7e5cb3391bee71d5623f2ecffd
Artur Sulej <artur@fresha.com>
2024-09-30 09:37:23 +0200

    RwLock

--- native/niflheim_nifdemo/src/lib.rs
+++ native/niflheim_nifdemo/src/lib.rs
@@ -1,6 +1,6 @@
 use rustler::Encoder;
 use rustler::{Env, Error, ResourceArc, Term};
-use std::sync::Mutex;
+use std::sync::RwLock;
 
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
@@ -16,13 +16,13 @@ mod atoms {
 }
 
 struct CollectionResource {
-    pub collection: Mutex<Vec<f64>>,
+    pub collection: RwLock<Vec<f64>>,
 }
 
 #[rustler::nif]
 fn init() -> Result<ResourceArc<CollectionResource>, Error> {
     let resource = ResourceArc::new(CollectionResource {
-        collection: Mutex::new(vec![1.1, 4.5, 6.7]),
+        collection: RwLock::new(vec![1.1, 4.5, 6.7]),
     });
 
     Ok(resource)
@@ -33,7 +33,7 @@ fn read(
     env: Env,
     resource: ResourceArc<CollectionResource>,
 ) -> Result<Term, Error> {
-    let resource_struct = resource.collection.try_lock().unwrap();
+    let resource_struct = resource.collection.read().unwrap();
 
     // Convert the collection to a Rustler term
     let collection: Vec<f64> = resource_struct.clone();
